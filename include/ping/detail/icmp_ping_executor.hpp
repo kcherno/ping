63afbca99a243b9d5b5f985545fe3edf9da5e696
Kirill Chernigovsky <k.chernigovsky@gmail.com>
2026-06-02 17:33:43 +0800

    add icmp_ping_executor

---
+++ include/ping/detail/icmp_ping_executor.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string_view>
+#include <chrono>
+
+#include "net/ipv4/icmp/icmp.hpp"
+
+#include "net/ipv4/endpoint.hpp"
+
+#include "ping/application.hpp"
+
+namespace ping::detail
+{
+    class icmp_ping_executor final :
+        public application::basic_application_executor
+    {
+    public:
+
+        icmp_ping_executor(
+            const application::configuration& configuration,
+            application::statistics&          statistics) :
+                destination_endpoint_ {
+                    configuration_.address
+                },
+
+                header_ {
+                    .type     = net::ipv4::icmp::header::type_enumerator::echo,
+                    .code     = 0,
+                    .checksum = 0,
+
+                    .echo_message {
+                        .identifier      = configuration_.identifier,
+                        .sequence_number = 0
+                    }
+                },
+
+                message_ {
+                    configuration.icmp_echo_message
+                },
+
+                response_timeout_ {
+                    configuration.response_timeout
+                },
+
+                statistics_ {
+                    statistics
+                }
+        {}
+
+        void execute() override;
+
+    private:
+
+        void ping_once();
+
+        bool verify_response(const net::ipv4::endpoint&, std::string_view);
+
+        net::ipv4::endpoint      destination_endpoint_;
+        net::ipv4::icmp::header  header_;
+        std::string_view         message_;
+        std::chrono::seconds     response_timeout_;
+        net::ipv4::icmp::socket  socket_;
+        application::statistics& statistics_;
+    };
+}
---
+++ src/ping/detail/icmp_ping_executor.cpp
@@ -0,0 +1,104 @@
+#include <system_error>
+#include <iostream>
+#include <chrono>
+#include <format>
+#include <limits>
+#include <string>
+#include <thread>
+
+#include "net/ipv4/icmp/header.hpp"
+
+#include "net/ipv4/endpoint.hpp"
+
+#include "ping/detail/icmp_ping_executor.hpp"
+#include "ping/detail/timer.hpp"
+
+void ping::detail::icmp_ping_executor::execute()
+{
+    std::string buffer;
+
+    net::ipv4::endpoint source_endpoint;
+
+    timer timer {
+        [&](std::error_code& error)
+        {
+            socket_.receive_from(error, source_endpoint, buffer);
+        }
+    };
+
+    socket_.open();
+
+    std::cout << std::format("ping {} with {}({}) bytes of data\n\n",
+        destination_endpoint_.address(),
+        message_.size(),
+        message_.size() + net::ipv4::icmp::header::echo_message_header_size);
+
+    while (header_.echo_message.sequence_number <
+               std::numeric_limits<std::uint16_t>::max())
+    {
+        ping_once();
+
+        const auto begin = std::chrono::steady_clock::now();
+
+        timer.start_countdown(response_timeout_);
+
+        const auto end = std::chrono::steady_clock::now();
+
+        if (not timer.is_expired())
+        {
+            if (verify_response(source_endpoint, buffer))
+            {
+                [[maybe_unused]] const auto response_time =
+                    std::chrono::duration_cast<std::chrono::milliseconds>(
+                        end - begin);
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds {1});
+
+        ++header_.echo_message.sequence_number;
+    }
+}
+
+void ping::detail::icmp_ping_executor::ping_once()
+{
+    socket_.send_to(
+        destination_endpoint_,
+        net::ipv4::icmp::make_icmp_message(header_, message_));
+
+    ++statistics_.sent_packets;
+
+    std::cout << std::format(
+        "sent {} bytes to {}: identifier={} sequence_number={}\n",
+        message_.size() + net::ipv4::icmp::header::echo_message_header_size,
+        destination_endpoint_.address(),
+        header_.echo_message.identifier,
+        header_.echo_message.sequence_number);
+}
+
+bool ping::detail::icmp_ping_executor::verify_response(
+    const net::ipv4::endpoint& source_endpoint, std::string_view data)
+{
+    if (destination_endpoint_.address() == source_endpoint.address())
+    {
+        const auto& [header, message] = net::ipv4::icmp::unpack_icmp_message(
+            net::ipv4::icmp::header::type_enumerator::echo_reply, data);
+
+        if (header_ == header && message_ == message)
+        {
+            ++statistics_.received_packets;
+
+            std::cout << std::format(
+                "received {} bytes from {}: "
+                "identifier={} sequence_number={}\n",
+                data.size(),
+                source_endpoint.address(),
+                header.echo_message.identifier,
+                header.echo_message.sequence_number);
+
+            return true;
+        }
+    }
+
+    return false;
+}
