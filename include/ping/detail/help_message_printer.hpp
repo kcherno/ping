47a3371a9442e8b09b7672053b1304fc416437d2
Kirill Chernigovsky <k.chernigovsky@gmail.com>
2026-06-10 18:45:40 +0800

    add help message printer

---
+++ include/ping/detail/help_message_printer.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <format>
+
+#include "options/grammar.hpp"
+
+#include "ping/generic/executor.hpp"
+
+#include "ping/application.hpp"
+
+namespace ping::detail
+{
+    class help_message_printer final : public generic::executor
+    {
+    public:
+
+        constexpr help_message_printer(
+            const options::grammar& options
+        ) noexcept :
+            options_ {options}
+        {}
+
+        void execute() override
+        {
+            std::cout << std::format(
+                "Usage: {} [<options>] <address>\n",
+                application::name()
+            );
+        }
+
+    private:
+
+        const options::grammar& options_;
+    };
+}
